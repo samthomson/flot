1811d0135518716d3918e1f091d1580eee5cbd18
Sam Thomson <samt@samt.st>
2014-04-16 00:38:35 +0200

    func def of items.php

--- flot_flot/core/items.php
+++ flot_flot/core/items.php
@@ -1,7 +1,8 @@
 <?php
 	# handles everything to do with the items, initiating and rendering
 
-	#properties: url, private, oncology, template, dynamic/static
+	# properties: url, private, oncology, template, dynamic/static
+	# methods: rebuild, update, add, edit, delete
 
 
     # initiate an item from the data in urls datastore
