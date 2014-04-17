bda2f4acfe1d5c027c463260efcf5e95b346516b
Sam Thomson <samt@samt.st>
2014-04-17 16:31:31 +0200

    more cleaning datastore now out of repo

--- flot_flot/datastore/urls.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$urls = '[{"url":"/", "item": "page", "id":"page_0"},
-	{"url":"/features/features.html", "item": "page", "id":"page_1"},
-	{"url":"/gettingstarted.html", "item": "page", "id":"page_2"},
-	{"url":"/about/", "item": "page", "id":"page_3"}]';
-?>
