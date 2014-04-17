39dffa049593911b1e61ff9259e55380fa9cc32d
Sam Thomson <samt@samt.st>
2014-04-17 20:22:40 +0200

    404s

--- flot_flot/core/flot.php
+++ flot_flot/core/flot.php
@@ -7,21 +7,28 @@
 	class Flot {
 
 		public $datastore;
+		public $error_handler;
 		public $s_base_path;
 
 		function __construct() {
 			$this->s_base_path = str_replace($_SERVER['SCRIPT_NAME'],"",str_replace("\\","/",$_SERVER['SCRIPT_FILENAME'])).'/';
 			$this->load_all_dependencies();
 			$this->datastore = new DataStore;
+			$this->error_handler = new ErrorHandler;
 		}
 
 		function create_item_from_url() {
-			$item_id = $this->datastore->get_current_url_data()->id;
-			// get the object representing the page requested
-			$item = $this->datastore->get_item_data($item_id);
 			
-			$o_item = new Item($item, $this->s_base_path);
-			$o_item->render();
+			$item = $this->datastore->get_current_url_data();
+			if($item){
+				// get the object representing the page requested
+				$item = $this->datastore->get_item_data($item->id);
+				
+				$o_item = new Item($item, $this->s_base_path);
+				$o_item->render();
+			}else{
+				$this->error_handler->throw_404();
+			}
 		}
 
 		function load_all_dependencies() {
