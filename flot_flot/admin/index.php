de2f6958e3426efc4382f4ad166735c7a6521ed9
Sam Thomson <samt@samt.st>
2014-04-20 00:52:02 +0200

    icons into back end and resp menu tabs

--- flot_flot/admin/index.php
+++ flot_flot/admin/index.php
@@ -41,8 +41,8 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">help</a></li>
-        <li><a href="logout.php">logout</a></li>
+        <li><a href="#"><i class="glyphicon glyphicon-question-sign"></i> help</a></li>
+        <li><a href="logout.php"><i class="glyphicon glyphicon-user"></i> logout</a></li>
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
@@ -52,11 +52,10 @@
       <!-- tabs left -->
       <div class="tabbable tabs-left">
         <ul class="nav nav-tabs">
-          <li class="active"><a href="#a" data-toggle="tab">Webpages</a></li>
-          <li><a href="#b" data-toggle="tab">Pictures</a></li>
-          <li><a href="#b" data-toggle="tab">Pictures</a></li>
-          <li><a href="#c" data-toggle="tab">Menus</a></li>
-          <li><a href="#d" data-toggle="tab">Settings</a></li>
+          <li class="active"><a href="#a" data-toggle="tab"><i class="glyphicon glyphicon-file"></i><span class="hidden-xs"> Webpages</span></a></li>
+          <li><a href="#b" data-toggle="tab"><i class="glyphicon glyphicon-picture"></i><span class="hidden-xs"> Pictures</span></a></li>
+          <li><a href="#c" data-toggle="tab"><i class="glyphicon glyphicon-list"></i><span class="hidden-xs"> Menus</span></a></li>
+          <li><a href="#d" data-toggle="tab"><i class="glyphicon glyphicon-cog"></i><span class="hidden-xs"> Settings</span></a></li>
         </ul>
         <div class="tab-content">
          <div class="tab-pane active" id="a">
