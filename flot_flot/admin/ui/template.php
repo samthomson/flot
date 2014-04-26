1b1db9d2675d3c4e91f6a989094158eab4ff2ed0
Sam Thomson <samt@samt.st>
2014-04-26 16:05:17 +0200

    more backend design changes

--- flot_flot/admin/ui/template.php
+++ flot_flot/admin/ui/template.php
@@ -33,7 +33,6 @@
 			</div>
 			<div class="right_col clearer" id="right_admin_section">
 				<!-- main 'content' section -->
-
 				<?php 
 					if($html_make_admin_content_menu !== ""){
 						echo '<div class="btn-group">'.$html_make_admin_content_menu.'</div>'; 
