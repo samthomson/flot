9d601bee6fea3f091509852142b6183638062c9a
Sam Thomson <samt@samt.st>
2014-05-03 16:28:34 +0200

    picture icon on wysiwyg picture insert button now

--- flot_flot/external_integrations/ckeditor/plugins/flot_pictures/plugin.js
+++ flot_flot/external_integrations/ckeditor/plugins/flot_pictures/plugin.js
@@ -13,8 +13,8 @@ CKEDITOR.plugins.add( 'flot_pictures',
 		editor.ui.addButton( 'Pictures',
 		{
 			label: 'Insert Picture',
-			command: 'insert_picture'/*,
-			icon: this.path + 'images/timestamp.png'*/
+			command: 'insert_picture',
+			icon: this.path + 'image.PNG'
 		} );
 	}
 } );
