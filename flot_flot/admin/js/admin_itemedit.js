d67dc3e2f7abebf612a47bb95ab2f867fe58ff5f
samthomson <samt@samt.st>
2014-05-10 21:23:19 +0200

    slug improvement, replaces spaces with hyphens and strips accents

--- flot_flot/admin/js/admin_itemedit.js
+++ flot_flot/admin/js/admin_itemedit.js
@@ -41,7 +41,7 @@ $(document).ready(function() {
     });
     $("#item_edit_url").keyup(function() {
         if(!$("#item_edit_auto_url").is(':checked')){
-            $("input[type=hidden].item_edit_url").val($("#item_edit_url").val());
+            $("input[type=hidden].item_edit_url").val(s_make_slug($("#item_edit_url").val()));
         }
     });
     $("#item_edit_auto_url").change(function() {
@@ -59,10 +59,23 @@ $(document).ready(function() {
 
 function _set_url_from_title(){
     //var s_slug = encodeURIComponent('/' + $("#item_edit_title").val() + '/');
-    var s_slug = '/' + $("#item_edit_title").val().toLowerCase() + '/';
+    var s_slug = '/' + s_make_slug($("#item_edit_title").val()) + '/';
     $(".item_edit_url").val(s_slug);
 }
+function s_make_slug(slugcontent)
+{
+    // found here: http://www.netvivs.com/build-permalinks-slug-with-javascript-jquery/
+    // convert to lowercase (important: since on next step special chars are defined in lowercase only)
+    slugcontent = slugcontent.toLowerCase();
+    // convert special chars
+    var   accents={a:/\u00e1/g,e:/u00e9/g,i:/\u00ed/g,o:/\u00f3/g,u:/\u00fa/g,n:/\u00f1/g}
+    for (var i in accents) slugcontent = slugcontent.replace(accents[i],i);
 
+    var slugcontent_hyphens = slugcontent.replace(/\s/g,'-');
+    var finishedslug = slugcontent_hyphens.replace(/[^a-zA-Z0-9\-]/g,'');
+    finishedslug = finishedslug.toLowerCase();
+    return finishedslug;
+}
 
 function publish(s_publish_status){
     console.log("set status to: " + s_publish_status);
