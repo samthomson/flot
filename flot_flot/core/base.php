b15e89af0f8fecd123b9abae274dfac6beabb5b9
samthomson <samt@samt.st>
2014-06-11 20:55:57 +0200

    a final updater test?

--- flot_flot/core/base.php
+++ flot_flot/core/base.php
@@ -17,7 +17,7 @@
 
 	@define('FLOT_VERSION_MAJOR', 0);
 	@define('FLOT_VERSION_MINOR', 3);
-	@define('FLOT_VERSION_BUILD', 16);
+	@define('FLOT_VERSION_BUILD', 17);
 
 
 	@define('FLOT_DOWNLOAD_URL', 'https://github.com/samthomson/flot/archive/master.zip');
