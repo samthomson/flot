ea0e7bbfd1c0fc410a6bdc6a3e45295ca203e4a8
unknown <samt@samt.st>
2015-10-18 14:58:05 +0200

    travis continued

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -7,7 +7,7 @@ class BaseTest extends PHPUnit_Framework_TestCase
      *
      * @return void
      */
-     /*
+    
     public function testBasicExample()
     {
         //$this->assertEquals(1,1);
@@ -16,13 +16,13 @@ class BaseTest extends PHPUnit_Framework_TestCase
         $session = new \Behat\Mink\Session($driver);
 
         $session->start();
-        $session->visit('http://localhost.dev/');
+        $session->visit('http://flot1.dev/');
         $page = $session->getPage();
 
         //$this->assertEquals($session->getStatusCode(),200);
         $this->assertEquals($page->getText(),'[to be generated]');
         //$this->visit('/')->see('[to be generated]');
-    }*/
+    }
     public function testPageModelCreate()
     {
         $oTestPage = PageModel::create();
